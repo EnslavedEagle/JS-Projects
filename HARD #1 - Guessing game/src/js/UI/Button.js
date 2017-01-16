b041c10d6f5451e1dc791456c94e48b3e06609fe
Patrky Bernasiewicz <kontakt@patrykb.pl>
2017-01-16 14:02:10 +0100

    Minor fixes Button class

--- HARD #1 - Guessing game/src/js/UI/Button.js
+++ HARD #1 - Guessing game/src/js/UI/Button.js
@@ -8,17 +8,6 @@ export class Button extends UIElement {
 		this.element.setAttribute('id', id);
 		this.element.classList.add('btn', className);
 	}
-	get() {
-		return this.element;
-	}
-	hide() {
-		this.element.setAttribute('disabled', 'disabled');
-		this.element.style.display = 'none';
-	}
-	show() {
-		this.element.removeAttribute('disabled');
-		this.element.style.dsiplay = 'inline-block';
-	}
 	event(event, callback) {
 		console.log('Adding event "' + event + '" for #' + this.element.getAttribute('id'), callback);
 		this.element.addEventListener(event, callback);
